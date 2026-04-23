323da5a24f35ca467c34f7e9a4c1e45310a6a2d8
yashmikavisha1-byte <yashmikavisha1@gmail.com>
2026-04-23 17:44:23 +0530

    Revert "add custom exception classes for API error handling"
    This reverts commit 85c41c49ab1d110b029e9c343d1f8150555f52da.

--- Smart-Campus-Api1/src/main/java/com/smart/campus/api1/exception/LinkedResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.smart.campus.api1.exception;
-
-public class LinkedResourceNotFoundException extends RuntimeException {
-
-    public LinkedResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- Smart-Campus-Api1/src/main/java/com/smart/campus/api1/exception/SensorUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package com.smart.campus.api1.exception;
-
-public class SensorUnavailableException extends RuntimeException {
-
-    public SensorUnavailableException(String sensorId) {
-        super("Sensor " + sensorId + " is currently under MAINTENANCE and cannot accept new readings.");
-    }
-}
