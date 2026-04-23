1b019c50a775992aca5f2049eece8d09e0c0b7e0
yashmikavisha1-byte <yashmikavisha1@gmail.com>
2026-04-23 16:47:36 +0530

    add exception mappers and request logging filter
    - implemented GlobalExceptionMapper for centralized error handling
    - added specific exception mappers for API responses
    - introduced LoggingFilter to log incoming requests and responses
    - improved consistency and debugging of API error handling

---
+++ Smart-Campus-Api1/src/main/java/com/smart/campus/api1/filter/LoggingFilter.java
@@ -0,0 +1,29 @@
+package com.smart.campus.api1.filter;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info(">>> REQUEST  : ["
+                + requestContext.getMethod()
+                + "] "
+                + requestContext.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext,
+                       ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info("<<< RESPONSE : Status = " + responseContext.getStatus());
+    }
+}
